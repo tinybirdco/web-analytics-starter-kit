ae43752b722d8887356e386674701ba925808d3d
Rafa Moreno <rmoreno@tinybird.co>
2022-09-09 06:13:50 +0000

    [Bugfix] Trigger inView callback only once (#18)
    * improve performance and web vitals
    
    * trigger inView callback once
    
    * fix type

--- dashboard/components/InView.tsx
+++ dashboard/components/InView.tsx
@@ -1,12 +1,13 @@
+import { ReactNode } from 'react'
 import { useInView } from 'react-intersection-observer'
 
 type InViewProps = {
-  children: JSX.Element
+  children: ReactNode
   height: number
 }
 
 export default function InView({ children, height }: InViewProps) {
-  const [ref, inView] = useInView({ threshold: 0 })
+  const [ref, inView] = useInView({ threshold: 0, triggerOnce: true })
   return (
     <div ref={ref} style={{ height }}>
       {inView && children}
