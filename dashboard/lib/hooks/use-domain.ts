06e8cdab029cba2c931c450132aa86f1ee4b8cdb
Alejandro Martin <alejandromav@tinybird.co>
2022-09-21 18:01:33 +0200

    Improve domain guessing

--- dashboard/lib/hooks/use-domain.ts
+++ dashboard/lib/hooks/use-domain.ts
@@ -4,14 +4,14 @@ import { querySQL } from '../api'
 import { DomainData, DomainQueryData } from '../types/domain'
 
 async function getDomain(): Promise<DomainData> {
-  const { data } = await querySQL<DomainQueryData>(
-    'SELECT href FROM analytics_hits ORDER BY timestamp DESC LIMIT 1 FORMAT JSON'
-  )
-  const results =
-    /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/g.exec(
-      data[0]['href']
-    )
-  const domain = results?.length ? results[1] : null
+  const { data } = await querySQL<DomainQueryData>(`
+    SELECT domainWithoutWWW(href) as domain
+    FROM analytics_hits
+    group by domain
+    order by count(1) desc
+    limit 1 FORMAT JSON
+  `)
+  const domain = data[0]['domain'];
   const logo = domain
     ? `https://s2.googleusercontent.com/s2/favicons?domain=${domain}`
     : FALLBACK_LOGO
