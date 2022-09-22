e4046ae9019a9ed7a3d0b36563bb9fb57a79c550
Rafa Moreno <rmoreno@tinybird.co>
2022-09-22 09:10:37 +0000

    fix credentials test

--- dashboard/__tests__/Credentials.test.tsx
+++ dashboard/__tests__/Credentials.test.tsx
@@ -1,20 +1,22 @@
 import { expect, test, vi } from 'vitest'
+import { useRouter } from 'next/router'
 import { render, screen, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Credentials from '../components/Credentials'
 
-test('Credentials', async () => {
+vi.mock('next/router', () => {
   const push = vi.fn()
-  const pathname = '/'
-
-  vi.mock('next/router', () => ({
+  return {
     useRouter: () => ({
       query: {},
       isReady: true,
       push,
-      pathname,
+      pathname: '/',
     }),
-  }))
+  }
+})
+
+test('Credentials', async () => {
   const token = 'my_token'
   render(<Credentials />)
   const form = within(screen.getByRole('form'))
@@ -22,5 +24,5 @@ test('Credentials', async () => {
   await userEvent.type(input, token)
   const button = form.getByRole('button', { name: /view dashboard/i })
   await userEvent.click(button)
-  expect(push).toHaveBeenCalledTimes(1)
+  expect(useRouter().push).toHaveBeenCalledTimes(1)
 })
