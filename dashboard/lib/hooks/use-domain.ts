b2578d6a835938d8d0c8dd6dc9a8e87c531f3b42
Alejandro Martin <alejandromav@tinybird.co>
2022-09-27 15:55:51 +0200

    Improve query performance

--- dashboard/lib/hooks/use-domain.ts
+++ dashboard/lib/hooks/use-domain.ts
@@ -4,13 +4,25 @@ import { querySQL } from '../api'
 import { DomainData, DomainQueryData } from '../types/domain'
 
 async function getDomain(): Promise<DomainData> {
+  // Guess the instrumented domain, and exclude other domains like development or staging.
+  //  - Try to get the domain with most hits from the last hour.
+  //  - Fallback to 'some' domain.
+  // Best balance between data accuracy and performance I can get.
   const { data } = await querySQL<DomainQueryData>(`
-    SELECT domainWithoutWWW(href) as domain
-    FROM analytics_hits
-    where timestamp >= today() - interval 7 day
-    group by domain
-    order by count(1) desc
-    limit 1 FORMAT JSON
+    with (
+      SELECT domainWithoutWWW(href) as domain
+      FROM analytics_hits
+      where timestamp >= now() - interval 1 hour
+      group by domain
+      order by count(1) desc
+      limit 1
+    ) as top_domain,
+    (
+      SELECT domainWithoutWWW(href)
+      FROM analytics_hits
+      limit 1
+    ) as some_domain
+    select coalesce(top_domain, some_domain) as domain
   `)
   const domain = data[0]['domain'];
   const logo = domain
