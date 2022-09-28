3bd8da7c7bebb3e9f4773dc63e659e1dfbf8cebd
Alejandro Martin <alejandromav@tinybird.co>
2022-09-28 10:39:38 +0200

    Remove Google favicon service references

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ['s2.googleusercontent.com'],
-  },
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
