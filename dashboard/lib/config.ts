2748f71b986e706789dad37dcd4316ee6a67b526
Rafa Moreno <rmoreno@tinybird.co>
2024-07-07 02:18:32 +0200

    replace charts

--- dashboard/lib/config.ts
+++ dashboard/lib/config.ts
@@ -1,8 +1,8 @@
 const config = {
-  dashboardURL: process.env.NEXT_PUBLIC_TINYBIRD_DASHBOARD_URL,
-  trackerToken: process.env.NEXT_PUBLIC_TINYBIRD_TRACKER_TOKEN,
-  authToken: process.env.NEXT_PUBLIC_TINYBIRD_AUTH_TOKEN,
-  host: process.env.NEXT_PUBLIC_TINYBIRD_HOST,
+  dashboardURL: process.env.NEXT_PUBLIC_TINYBIRD_DASHBOARD_URL as string,
+  trackerToken: process.env.NEXT_PUBLIC_TINYBIRD_TRACKER_TOKEN as string,
+  authToken: process.env.NEXT_PUBLIC_TINYBIRD_AUTH_TOKEN as string,
+  host: process.env.NEXT_PUBLIC_TINYBIRD_HOST as string,
 } as const
 
 export default config
