eaa02c9963daa0ca91eb0008df74748ee9c04e39
Rafa Moreno <rmoreno@tinybird.co>
2024-09-23 23:40:27 +0200

    change icons

--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -22,12 +22,25 @@ export default function DashboardPage() {
         />
       )}
       <Meta />
+      <div className="h-12 bg-primary text-sm leading-5 text-secondary flex items-center justify-center gap-2">
+        📊{' '}
+        <div>
+          <span className="font-semibold">Tinybird Charts</span>: Create
+          in-product analytics or internal dashboards in minutes.{' '}
+          <a
+            href="https://www.tinybird.co/docs/publish/charts"
+            className="underline"
+          >
+            Learn more
+          </a>
+        </div>
+      </div>
       <div className="min-h-screen px-5 py-5 text-sm leading-5 bg-body sm:px-10 text-secondary">
         <div className="mx-auto max-w-7xl">
           <div className="space-y-6 sm:space-y-10">
             {isAuthenticated && isTokenValid && (
               <>
-                <img src="/icon.png" alt="" width={24} height={24} />
+                <img src="/icon.svg" alt="" width={24} height={24} />
                 <Header />
               </>
             )}
