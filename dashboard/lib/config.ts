b0f279abf70d17343aa171900879d504da2107a1
alrocar <alrocar@users.noreply.github.com>
2025-07-17 11:32:37 +0200

    Multi domain3 (#167)
    * feat: add tenant ID and JWT creation
    
    * update tests

--- dashboard/lib/config.ts
+++ dashboard/lib/config.ts
@@ -3,6 +3,7 @@ const config = {
   trackerToken: process.env.NEXT_PUBLIC_TINYBIRD_TRACKER_TOKEN as string,
   authToken: process.env.NEXT_PUBLIC_TINYBIRD_AUTH_TOKEN as string,
   host: process.env.NEXT_PUBLIC_TINYBIRD_HOST as string,
+  tenantId: process.env.TENANT_ID as string,
 } as const
 
 export default config
